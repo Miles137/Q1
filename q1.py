e0f8a0cd6e9e8edda373d008631de113d4a580b5
Miles Tiley <miles.tiley99@gmail.com>
2019-10-09 16:14:09 +1100

    Initial commit

---
+++ q1.py
@@ -0,0 +1,15 @@
+# Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+# between 2000 and 3200 (both included).
+# The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+nums = list(range(2000,3201))
+## did not need to make this list separately: can just iterate through them in the for loop
+## do not need to make a list of the numbers: can just return them with comma separation via join
+
+for num in nums:
+#    print('anynum = ', num, 'bool = ', (num%7 != 0) or (num%5 == 0))
+    if (num%7 != 0) or (num%5 == 0):
+#        print('num = ', num, 'bool = ', (num%7 != 0) or (num%5 == 0))
+        nums.remove(num)
+        
+print (nums)
